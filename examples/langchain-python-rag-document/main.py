bffbed4df408cca4f89d383efad03731328c622e
NavidHeisenberg <45155036+NavidHeisenberg@users.noreply.github.com>
2024-05-29 22:45:36 -0400

    Update main.py
    The original code did not work for my set up. Replaced GPT4AllEmbeddings with OllamaEmbeddings.

--- examples/langchain-python-rag-document/main.py
+++ examples/langchain-python-rag-document/main.py
@@ -1,13 +1,17 @@
-from langchain.document_loaders import OnlinePDFLoader
-from langchain.vectorstores import Chroma
-from langchain.embeddings import GPT4AllEmbeddings
-from langchain import PromptTemplate
-from langchain.llms import Ollama
+from langchain_community.document_loaders import OnlinePDFLoader
+from langchain_community.vectorstores import Chroma
+# from langchain_community.embeddings import GPT4AllEmbeddings
+from langchain_community import embeddings
+from langchain_core.prompts import PromptTemplate
+from langchain_community.llms import Ollama
 from langchain.callbacks.manager import CallbackManager
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 from langchain.chains import RetrievalQA
+from langchain_community.embeddings import OllamaEmbeddings
 import sys
 import os
+import pdfminer
+import pdfminer.high_level
 
 class SuppressStdout:
     def __enter__(self):
@@ -22,15 +26,18 @@ class SuppressStdout:
         sys.stderr = self._original_stderr
 
 # load the pdf and split it into chunks
-loader = OnlinePDFLoader("https://d18rn0p25nwr6d.cloudfront.net/CIK-0001813756/975b3e9b-268e-4798-a9e4-2a9a7c92dc10.pdf")
-data = loader.load()
+loader = OnlinePDFLoader("https://d18rn0p25nwr6d.cloudfront.net/CIK-0001813756/975b>data = loader.load()
 
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=0)
 all_splits = text_splitter.split_documents(data)
 
+# Navid changed this line:
 with SuppressStdout():
-    vectorstore = Chroma.from_documents(documents=all_splits, embedding=GPT4AllEmbeddings())
+    vectorstore = Chroma.from_documents(
+        documents=all_splits,
+        embedding=embeddings.ollama.OllamaEmbeddings(model="nomic-embed-text"),
+    )
 
 while True:
     query = input("\nQuery: ")
@@ -40,9 +47,7 @@ while True:
         continue
 
     # Prompt
-    template = """Use the following pieces of context to answer the question at the end.
-    If you don't know the answer, just say that you don't know, don't try to make up an answer.
-    Use three sentences maximum and keep the answer as concise as possible.
+    template = """Use the following pieces of context to answer the question at the>    If you don't know the answer, just say that you don't know, don't try to make u>    Use three sentences maximum and keep the answer as concise as possible.
     {context}
     Question: {question}
     Helpful Answer:"""
@@ -51,8 +56,7 @@ while True:
         template=template,
     )
 
-    llm = Ollama(model="llama3:8b", callback_manager=CallbackManager([StreamingStdOutCallbackHandler()]))
-    qa_chain = RetrievalQA.from_chain_type(
+    llm = Ollama(model="llama3:8b", callback_manager=CallbackManager([StreamingStdO>    qa_chain = RetrievalQA.from_chain_type(
         llm,
         retriever=vectorstore.as_retriever(),
         chain_type_kwargs={"prompt": QA_CHAIN_PROMPT},
